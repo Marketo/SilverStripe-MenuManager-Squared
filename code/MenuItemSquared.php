6a7a3b6da573dcdfe8c3a70e5d790235de0a7557
Brandon Marshall <kem5brandon@gmail.com>
2017-02-22 18:49:15 -0800

    Add Max Depth for Sub Menu Items

--- code/MenuItemSquared.php
+++ code/MenuItemSquared.php
@@ -7,30 +7,67 @@ class MenuItemSquared extends DataExtension {
 
   private static $has_one = [
     'Image' => 'Image',
-    'MenuItem' => 'MenuItem',
+    'ParentItem' => 'MenuItem',
   ];
   
-  private static $has_many = array(
-    'MenuItems' => 'MenuItem'
-  );
+  private static $has_many = [
+    'ChildItems' => 'MenuItem',
+  ];
 
   public function updateCMSFields( FieldList $fields ) {
     $fields->push(new UploadField('Image', 'Image'));
+
     if ($this->owner->ID != null) {
-      $fields->push(
-          $menuItems = new GridField(
-              'MenuItems',
-              'Sub Menu Items',
-              $this->owner->MenuItems(),
-              $config = GridFieldConfig_RelationEditor::create()
+      $AllParentItems = $this->owner->getAllParentItems();
+      $TopMenuSet = $this->owner->TopMenuSet();
+      $depth = 1;
+
+      if(
+        is_array(MenuSet::config()->{$TopMenuSet->Name}) &&
+        isset(MenuSet::config()->{$TopMenuSet->Name}['depth']) &&
+        is_numeric(MenuSet::config()->{$TopMenuSet->Name}['depth']) &&
+        MenuSet::config()->{$TopMenuSet->Name}['depth'] > 1
+      ){
+        $depth = MenuSet::config()->{$TopMenuSet->Name}['depth'];
+      }
+      
+      if(!empty($AllParentItems) && count($AllParentItems) > $depth){
+        $fields->push(new LabelField('MenuItems', 'Max Sub Menu Depth Limit'));
+      } else {
+        $fields->push(
+          new GridField(
+            'MenuItems',
+            'Sub Menu Items',
+            $this->owner->ChildItems(),
+            $config = GridFieldConfig_RecordEditor::create()
           )
-      );
-      $config->addComponent(new GridFieldOrderableRows('Sort'));
-    } 
-    else {
+        );
+        $config->addComponent(new GridFieldOrderableRows('Sort'));
+      }
+    } else {
       $fields->push(new LabelField('MenuItems', 'Save This Menu Item Before Adding Sub Menu Items'));
     }
   }
+  
+  public function TopMenuSet() {
+      $AllParentItems = $this->owner->getAllParentItems();
+      if(!empty($AllParentItems)){
+        return end($AllParentItems)->MenuSet();
+      }
+      return $this->owner->MenuSet();
+  }
+  
+  public function getAllParentItems() {
+    $WorkingItem = $this->owner;
+    $ParentItems = [];
+    
+    while ($WorkingItem->ParentItemID && $WorkingItem->ParentItem() && $WorkingItem->ParentItem()->ID && !isset($ParentItems[$WorkingItem->ParentItem()->ID])) {
+      $ParentItems[$WorkingItem->ID] = $WorkingItem->ParentItem();
+      $WorkingItem = $ParentItems[$WorkingItem->ID];
+    }
+    return $ParentItems;
+  }
+  
   public function onBeforeWrite() {
       if (!$this->owner->Sort) {
           $this->owner->Sort = MenuItem::get()->max('Sort') + 1;
